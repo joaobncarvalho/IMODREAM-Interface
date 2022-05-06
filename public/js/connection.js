d4795c6a988b71dd851297876547152d071ff634
João Carvalho <joaove>
2022-05-06 11:15:42 +0100

    push

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,21 +1,22 @@
 $(document).ready(
 
     function(){
+
         $.ajax({
-            url: "https://imodream-api.herokuapp.com/api/proprety",
+            url: "https://imodream-api.herokuapp.com/api/proprety/one",
             type: "GET",
             dataType: 'json',
             success: function(result) {
                 console.log(result);
                 $('#Moradia_Tipo').text(result)
                 var down = document.getElementById("Moradia_Tipo");
-                var obj = JSON.stringify(result);
                 var res = [""];
-
+                var obj = JSON.stringify(result);
                 for(var i in obj)
                     res.push(obj[i]);
 
-              //  down.innerHTML =  ;
+                down.innerHTML = obj[5]+obj[6];
+
                 {
 
                 }
