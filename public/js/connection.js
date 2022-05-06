384a103742641c2ef944cc7ec7e095c041346063
João Carvalho <joaove>
2022-05-06 20:54:13 +0100

    push

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,34 +1,54 @@
 $(document).ready(
 
-    function(){
-
+    function house1(){
         $.ajax({
             url: "https://imodream-api.herokuapp.com/api/proprety/one",
             type: "GET",
             dataType: 'json',
             success: function(result) {
                 console.log(result);
-                $('#Moradia_Tipo').text(result)
-                var down = document.getElementById("Moradia_Tipo");
-                var res = [""];
+                $('#DimensionPro').text(result)
+                var down = document.getElementById("list");
                 var obj = JSON.stringify(result);
                 console.log(obj);
-                for(var i in obj)
+                var res = [""];
+
+                for(var i in result) {
                     res.push(obj[i]);
-
-                down.innerHTML = obj[5]+obj[6];
-
-                {
-
+                    //down.innerHTML = obj[i];
+                    let h2 = document.createElement("p");
+                    h2.innerText = result[i].Dimension + " - " + result[i].Measurments;
+                    down.appendChild(h2);
                 }
             }
 
         });
     }
 );
+$(document).ready(
+    function house12(){
+        $.ajax({
+        url: "https://imodream-api.herokuapp.com/api/proprety/one",
+        type: "GET",
+        dataType: 'json',
+        success: function(result) {
+            console.log(result);
+            $('#LocPro').text(result)
+            var down = document.getElementById("h12");
+            var obj = JSON.stringify(result);
+            console.log(obj);
+            var res = [""];
 
+            for(var i in result) {
+                res.push(obj[i]);
+                //down.innerHTML = obj[i];
+                let h2 = document.createElement("i");
+                h2.innerText = result[i].StreetName;
+                down.appendChild(h2);
+                }
+            }
 
+        });
+    }
 
-
-
-
+);
