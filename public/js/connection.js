c129d49db7c126db101788c00b8b5495a3648ccb
João Carvalho <joaove>
2022-05-07 16:07:35 +0100

    push

--- public/js/connection.js
+++ public/js/connection.js
@@ -42,9 +42,38 @@ $(document).ready(
             for(var i in result) {
                 res.push(obj[i]);
                 //down.innerHTML = obj[i];
-                let h2 = document.createElement("i");
-                h2.innerText = result[i].StreetName;
-                down.appendChild(h2);
+                let h12 = document.createElement("i");
+                h12.innerText = result[i].StreetName;
+                down.appendChild(h12);
+                }
+            }
+
+        });
+    }
+
+);
+
+
+$(document).ready(
+    function house13(){
+        $.ajax({
+            url: "https://imodream-api.herokuapp.com/api/proprety/one",
+            type: "GET",
+            dataType: 'json',
+            success: function(result) {
+                console.log(result);
+                $('#PricePro').text(result)
+                var down = document.getElementById("h13");
+                var obj = JSON.stringify(result);
+                console.log(obj);
+                var res = [""];
+
+                for(var i in result) {
+                    res.push(obj[i]);
+                    //down.innerHTML = obj[i];
+                    let h13 = document.createElement("h");
+                    h13.innerText = result[i].Price;
+                    down.appendChild(h13);
                 }
             }
 
