55ccf14ee67566e4968a37893adfbbcfa91820ef
João Carvalho <joaove>
2022-05-12 12:05:34 +0100

    push

--- public/js/jsmapa.js
+++ public/js/jsmapa.js
@@ -7,11 +7,35 @@ function initMap(lat,long) {
         zoom: 13,
         center: { lat: lat, lng: long },
     });
+    console.log(lat,long)
+    $.ajax({
+        url: "https://imodream-api.herokuapp.com/api/proprety",
+        type: "GET",
+        dataType: 'json',
+        success: function(result) {
+            //console.log(result);
+            $('#DimensionPro').text(result)
+            var down = document.getElementById("map");
+            //console.log(obj);
+
+            var res = [""];
+
+            for (var i=0; i < result.length-1;i++){
+                //console.log(result[i].location)
+                lat = result[i].location.coordinates[0]
+                long = result[i].location.coordinates[1]
+
+                initMap(lat,long);
+
+            }
 
 
+        }
+
+    })
     const image =
         "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-    const beachMarker = new google.maps.Marker({
+    const beachMarker = new google.maps.LatLng({
         position: { lat: lat, lng: long },
         map,
         icon: image,
@@ -25,42 +49,9 @@ window.initMap = initMap;
 
 
 
-//$(document).ready(
-
-     function MarkersLoc(){
-         $.ajax({
-            url: "https://imodream-api.herokuapp.com/api/proprety",
-            type: "GET",
-            dataType: 'json',
-            success: function(result) {
-                //console.log(result);
-                $('#DimensionPro').text(result)
-                var down = document.getElementById("map");
-                //console.log(obj);
-
-                var res = [""];
-
-                for (var i=0; i < result.length-1;i++){
-                    //console.log(result[i].location)
-                    lat = result[i].location.coordinates[0]
-                    long = result[i].location.coordinates[1]
-
-                    initMap(lat,long);
-
-                }
-
-
-            }
-
-        })
-    }
-
-
 
 
 
 
 
 //);
-
-
